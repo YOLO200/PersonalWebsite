6dddc83465dc88c0af4973978d9779f77c69b004
Yashvi <yashvi8jasani@gmail.com>
2025-11-25 17:33:31 -0700

    SEO

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8d65293e0f751e99")
+/******/ 		__webpack_require__.h = () => ("a423d33a0967ec1d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66ae80218010b5bf"; }
+/******/ 		__webpack_require__.h = function() { return "6e2ecfd53ddf05cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
